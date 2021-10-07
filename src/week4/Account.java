8552f1c4613de154a877b7f3118b37d3d39a1cf1
Ilya Malakhov <malakhov.ilya.pavlovich@yandex.ru>
2021-10-07 10:04:29 +0700

    week4 initial commit

---
+++ src/week4/Account.java
@@ -0,0 +1,12 @@
+package week4;
+
+
+import java.util.Date;
+
+public class Account {
+    private String fullName;
+    private Date dateOfBirth;
+    private String email;
+    private int password;
+    private boolean blocked;
+}
